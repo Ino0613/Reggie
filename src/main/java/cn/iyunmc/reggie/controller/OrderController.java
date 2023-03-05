2084da8151b0c280a2d137bc2db2e4f5a66e41fd
BuildTools <1252343981@qq.com>
2023-03-05 17:02:52 +0800

    Reggie

--- src/main/java/cn/iyunmc/reggie/controller/OrderController.java
+++ src/main/java/cn/iyunmc/reggie/controller/OrderController.java
@@ -1,14 +1,22 @@
 package cn.iyunmc.reggie.controller;
 
+import cn.iyunmc.reggie.common.BaseContext;
 import cn.iyunmc.reggie.common.R;
+import cn.iyunmc.reggie.entity.OrderDetail;
 import cn.iyunmc.reggie.entity.Orders;
+import cn.iyunmc.reggie.entity.ShoppingCart;
+import cn.iyunmc.reggie.service.OrderDetailService;
 import cn.iyunmc.reggie.service.OrderService;
+import cn.iyunmc.reggie.service.ShoppingCartService;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @Slf4j
@@ -17,11 +25,32 @@ public class OrderController {
 
     @Autowired
     private OrderService orderService;
+    @Autowired
+    private OrderDetailService orderDetailService;
+
+    @Autowired
+    private ShoppingCartService shoppingCartService;
 
     @PostMapping("/submit")
     public R<String> submit(@RequestBody Orders orders) {
         log.info("order:{}", orders);
         orderService.submit(orders);
         return R.success("Test");
+    }
+
+    @GetMapping("/userPage")
+    public R<IPage<OrderDetail>> getPage(@RequestParam int page, @RequestParam int pageSize) {
+        log.info("信息查询：{}，{}", page, pageSize);
+        Page<OrderDetail> page1 = new Page<>(page, pageSize);
+        Long userId = BaseContext.getCurrenId();
+        LambdaQueryWrapper<OrderDetail> lqw = new LambdaQueryWrapper<>();
+        LambdaQueryWrapper<Orders> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(Orders::getUserId, userId);
+        List<Orders> orders = orderService.list(queryWrapper);
+        List<Long> list = orders.stream().map(Orders::getId).collect(Collectors.toList());
+        lqw.in(OrderDetail::getOrderId, list);
+
+        orderDetailService.page(page1, lqw);
+        return R.success(page1);
     }
 }
--- src/main/java/cn/iyunmc/reggie/service/impl/OrderServiceImpl.java
+++ src/main/java/cn/iyunmc/reggie/service/impl/OrderServiceImpl.java
@@ -2,21 +2,26 @@ package cn.iyunmc.reggie.service.impl;
 
 import cn.iyunmc.reggie.common.BaseContext;
 import cn.iyunmc.reggie.common.CustomException;
-import cn.iyunmc.reggie.entity.AddressBook;
-import cn.iyunmc.reggie.entity.Orders;
-import cn.iyunmc.reggie.entity.ShoppingCart;
+import cn.iyunmc.reggie.common.R;
+import cn.iyunmc.reggie.entity.*;
 import cn.iyunmc.reggie.mapper.OrderMapper;
-import cn.iyunmc.reggie.service.AddressBookService;
-import cn.iyunmc.reggie.service.OrderService;
-import cn.iyunmc.reggie.service.ShoppingCartService;
-import cn.iyunmc.reggie.service.UserService;
+import cn.iyunmc.reggie.service.*;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.IdWorker;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 @Service
 public class OrderServiceImpl extends ServiceImpl<OrderMapper, Orders> implements OrderService {
@@ -29,18 +34,23 @@ public class OrderServiceImpl extends ServiceImpl<OrderMapper, Orders> implement
 
     @Autowired
     private AddressBookService addressBookService;
+
+    @Autowired
+    private OrderDetailService orderDetailService;
+
     /**
      * 用户下单
+     *
      * @param orders
      */
     @Transactional
     @Override
     public void submit(Orders orders) {
         //获得当前用户id
-        Long currenId = BaseContext.getCurrenId();
+        Long userId = BaseContext.getCurrenId();
         //查询当前用户的购物车数据
         LambdaQueryWrapper<ShoppingCart> lqw = new LambdaQueryWrapper<>();
-        lqw.eq(ShoppingCart::getUserId, currenId);
+        lqw.eq(ShoppingCart::getUserId, userId);
         List<ShoppingCart> shoppingCarts = shoppingCartService.list(lqw);
 
         if (shoppingCarts == null || shoppingCarts.size() == 0) {
@@ -48,11 +58,53 @@ public class OrderServiceImpl extends ServiceImpl<OrderMapper, Orders> implement
         }
 
         //查询用户数据
+        User user = userService.getById(userId);
+        //查询地址数据
+        Long addressBookId = orders.getAddressBookId();
+        AddressBook addressBook = addressBookService.getById(addressBookId);
+        if (addressBook == null) {
+            throw new CustomException("用户地址信息有误，不能下单");
+        }
 
+        long orderId = IdWorker.getId(); //订单号
 
+        AtomicInteger amount = new AtomicInteger(0);
+        List<OrderDetail> orderDetails = shoppingCarts.stream().map((item) -> {
+            OrderDetail orderDetail = new OrderDetail();
+            orderDetail.setOrderId(orderId);
+            orderDetail.setNumber(item.getNumber());
+            orderDetail.setDishFlavor(item.getDishFlavor());
+            orderDetail.setDishId(item.getDishId());
+            orderDetail.setSetmealId(item.getSetmealId());
+            orderDetail.setName(item.getName());
+            orderDetail.setImage(item.getImage());
+            orderDetail.setAmount(item.getAmount());
+            amount.addAndGet(item.getAmount().multiply(new BigDecimal(item.getNumber())).intValue());
+            return orderDetail;
+        }).collect(Collectors.toList());
         //向订单表插入数据，一条数据
+        orders.setId(orderId);
+        orders.setOrderTime(LocalDateTime.now());
+        orders.setCheckoutTime(LocalDateTime.now());
+        orders.setStatus(2);
+        orders.setAmount(new BigDecimal(amount.get()));//总金额
+        orders.setUserId(userId);
+        orders.setNumber(String.valueOf(orderId));
+        orders.setUserName(user.getName());
+        orders.setConsignee(addressBook.getConsignee());
+        orders.setPhone(addressBook.getPhone());
+        orders.setAddress((addressBook.getProvinceName() == null ? "" : addressBook.getProvinceName())
+                + (addressBook.getCityName() == null ? "" : addressBook.getCityName())
+                + (addressBook.getDistrictName() == null ? "" : addressBook.getDistrictName())
+                + (addressBook.getDetail() == null ? "" : addressBook.getDetail()));
+        orders.setNumber(String.valueOf(orderId));
         this.save(orders);
         //向订单明细表插入数据
+        orderDetailService.saveBatch(orderDetails);
 
+        //清空购物车数据
+        shoppingCartService.remove(lqw);
     }
+
+
 }
